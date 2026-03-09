938820b4a47fa2cfcb3575746077f3de573fb20d
Geoffrey Eisenbarth <geoffrey.eisenbarth@gmail.com>
2026-03-09 10:16:35 -0500

    Add pre-commit css linting

--- .config/mise/tasks/lint/css.ts
+++ .config/mise/tasks/lint/css.ts
@@ -5,11 +5,18 @@ import standardConfig from 'npm:stylelint-config-standard';
 import stylistic from 'npm:@stylistic/stylelint-plugin';
 import logicalPlugin from 'npm:stylelint-use-logical';
 import browserCompat from 'npm:stylelint-browser-compat';
+import orderPlugin from 'npm:stylelint-order';
+import propertyGroups from 'npm:stylelint-config-recess-order/groups';
 import { resolve } from 'jsr:@std/path/resolve'
+
+const cssFiles = Deno.env.has('STAGED')
+  ? Deno.env.get('STAGED').split(' ').filter(fn => fn.endsWith('.css'))
+  : ['src/**/*.css']
+if (cssFiles.length === 0) Deno.exit()
 
 const result = await stylelint.lint({
   cwd: resolve(import.meta.dirname, '../../../..'),
-  files: 'src/**/*.css',
+  files: cssFiles,
   formatter: 'string',
   fix: true,
   config: {
@@ -17,22 +24,30 @@ const result = await stylelint.lint({
       stylistic,
       logicalPlugin,
       browserCompat,
+      orderPlugin,
     ],
     rules: {
       // ...standardConfig.rules,
       'selector-attribute-quotes': 'never',
       'selector-class-pattern': '^(\\<|-|[a-z])([a-z0-9]|\\:|@|-|\\%)*(\\>)?$',
-
+      'block-no-empty': null,
       '@stylistic/indentation': 2,
       '@stylistic/declaration-colon-space-after': null,
       '@stylistic/no-eol-whitespace': true,
       '@stylistic/block-closing-brace-empty-line-before': 'never',
 
+      // Disable auto-fix for the following:
       'declaration-block-no-redundant-longhand-properties': [true, { disableFix: true }],
       'declaration-block-no-duplicate-properties': [true, { disableFix: true }],
       'declaration-block-no-duplicate-custom-properties': [true, { disableFix: true }],
       'no-duplicate-selectors': [true, { disableFix: true }],
       'csstools/use-logical': [true, { disableFix: true }],
+
+      /** WIP: Use recess ordering
+      'declaration-empty-line-before': 'never',
+      'custom-property-empty-line-before': 'never',
+      'order/properties-order': propertyGroups,
+      */
 
       'plugin/browser-compat': [
         true,
